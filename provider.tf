b746ac8bff90b2908e5cad3743eebbf34519d299
Diego Barcelos Nicacio <diego.nicacio@mandic.net.br>
2020-01-28 20:02:02 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,14 @@
 #--------------------------------------------------------------------------------------------------------------
 # CREDENTIALS                                                                                                 |
 #--------------------------------------------------------------------------------------------------------------
+##provider "aws" {
+##  region = "us-east-1"
+##  shared_credentials_file = "~/.aws/credentials"
+##  profile = "diego.nicacio"
+##}
+
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile = "diego.nicacio"
+  aws_access_key_id = "$aws_access_key_id"
+  aws_secret_access_key = "$aws_secret_access_key"
 }
