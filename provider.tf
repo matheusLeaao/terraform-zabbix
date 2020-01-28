010820089f5374d18140efc591c3b12f885846bd
Diego Barcelos Nicacio <diego.nicacio@mandic.net.br>
2020-01-28 20:41:46 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 # CREDENTIALS                                                                                                 |
 #--------------------------------------------------------------------------------------------------------------
 provider "aws" {
-  AWS_DEFAULT_REGION="$REGION"
+  AWS_REGION="$REGION"
   AWS_ACCESS_KEY_ID="$ACCESS_KEY"
   AWS_SECRET_ACCESS_KEY="$SECRET_KEY"
 }
